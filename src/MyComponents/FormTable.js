be0c2abb7fcd0cfa1c46e5604f65ce46d9cc087a
RajanMhrzn <60804591+RajanMhrzn@users.noreply.github.com>
2022-02-24 21:27:55 +0545

    ui modification

--- src/MyComponents/FormTable.js
+++ src/MyComponents/FormTable.js
@@ -1,21 +1,49 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import './Table.css';
 export const FormTable = ({ dataArray }) => {
   return (
-    <div className="container text-center">
-      <h3>Table List</h3>
-      {/* <Link to="/Profiles"> Profile</Link> */}
-      <table>
-        <thead>
+    // <div className="container text-center">
+    //   <h3>Table List</h3>
+    //   {/* <Link to="/Profiles"> Profile</Link> */}
+    //   <table>
+    //     <thead>
+          // <tr>
+          //   <th>S.No</th>
+          //   <th>Name</th>
+          //   <th>Email</th>
+          //   <th>Phone</th>
+          // </tr>
+    //     </thead>
+    //     <tbody>
+    //         {dataArray.map((val) => {
+    //         return (
+    //           <tr key={val.id}>
+    //             <td>{val.id}</td>
+    //             <td>{val.uname}</td>
+    //             <td>{val.email}</td>
+    //             <td>{val.phone}</td>
+    //             <td>
+                 
+    //             </td>
+    //           </tr>
+    //         );
+    //     })}
+    //     </tbody>
+    //   </table>
+    // </div>
+
+    <table class="table">
+  <thead>
           <tr>
             <th>S.No</th>
             <th>Name</th>
             <th>Email</th>
             <th>Phone</th>
           </tr>
-        </thead>
-        <tbody>
-            {dataArray.map((val) => {
+  </thead>
+  <tbody>
+
+          {dataArray.map((val) => {
             return (
               <tr key={val.id}>
                 <td>{val.id}</td>
@@ -28,8 +56,9 @@ export const FormTable = ({ dataArray }) => {
               </tr>
             );
         })}
-        </tbody>
-      </table>
-    </div>
+    
+  </tbody>
+</table>
+
   );
 };
